b7a3f6aa8cefa58756d4358bb72a45dcfaeb97c1
Ben Collins <aggieben@gmail.com>
2018-07-25 10:45:57 -0500

    First stab at an improved document model.

---
+++ src/Toml.FSharp/DocumentModel.fs
@@ -0,0 +1,52 @@
+namespace Toml
+
+open System
+open System.Collections.ObjectModel
+
+type PrimitiveValue =
+    | String of string
+    | Integer of int64
+    | Float of float
+    | Boolean of bool
+    | OffsetDateTime of DateTimeOffset
+    | LocalDateTime of DateTime
+    | LocalDate of Date
+    | LocalTime of Time
+
+type ArrayValue = Array of PrimitiveValue
+
+type Value =
+    | PrimitiveValue of PrimitiveValue
+    | ArrayValue of ArrayValue
+
+/// This type is intentionally flat, rather than recursive.  While it can be reasonably said that TOML supports
+/// nested tables, it can equally be said that nested tables can be represented in a flat structure with
+/// "dot notation" naming.  For example:
+/// 
+/// ```
+/// [a]
+/// key = value
+///   [b]
+///   key = value
+/// ```
+/// 
+/// This structure could be represented as nested tables.  However, I think in most actual use cases it will be
+/// simpler and safer to conceptually represent them like this:
+/// 
+/// ```
+/// [a]
+/// key = value
+/// 
+/// [a.b]
+/// key = value
+/// ```
+/// 
+/// This is not to mean that users should not nest tables - far from it.  This is simply to say that the Document Model
+/// will represent them as flat structures, and leave nesting as a problem for parsing.
+type Table =
+    | SimpleTable of ReadOnlyDictionary<string,Value>
+    | TableArray of ReadOnlyDictionary<string,Value> array
+
+type Document = {
+    Tables : Table array option; // top-level KVPs can be represented in the "default" or ""-named table
+}
