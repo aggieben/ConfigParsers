312597c81a9b200295878fd68774317d6da8542f
Ben Collins <aggieben@gmail.com>
2018-07-28 15:52:46 -0500

    swap in Xunit (and importantly, FsCheck.Xunit).  Found a bug.

--- tests/FPConfig.Toml.Tests/ParserTests.fs
+++ tests/FPConfig.Toml.Tests/ParserTests.fs
@@ -1,18 +1,8 @@
-﻿#if INTERACTIVE
-#r @"../../packages/NUnit/lib/net45/nunit.framework.dll"
-#r @"../../packages/FParsec/lib/net40-client/FParsecCS.dll"
-#r @"../../packages/FParsec/lib/net40-client/FParsec.dll"
-#r @"../../packages/FsCheck/lib/net45/FsCheck.dll"
-#r @"../../src/toml-fs/bin/release/toml-fs.dll"
-#load   "Prelude.fs"
-        "Generators.fs"
-#else
-module FPConfig.Toml.Tests.ParserTests
-#endif
+﻿module FPConfig.Toml.Tests.ParserTests
 
-
-open NUnit.Framework
+open Xunit
 open FsCheck
+open FsCheck.Xunit
 open FParsec 
 open FPConfig.Toml.Parsers
 open FPConfig.Toml.Tests.Generators
@@ -21,13 +11,7 @@ open FPConfig.Toml.Tests.Prelude
 let inline throwConfig maxTest startSize endSize = 
     { Config.QuickThrowOnFailure with MaxTest = maxTest; StartSize = startSize; EndSize = endSize}
 
-let longCheck   x = Check.One (throwConfig 10000 50 700, x)
-let midCheck    x = Check.One (throwConfig 3000  20 300, x)
-let shortCheck  x = Check.One (throwConfig 500   5  100, x)
-
-(*|---------------------|*)
-(*| String Parser Tests |*)
-(*|---------------------|*)
+// string parser tests
 
 let parserTest bound parser  =
     fun (str:string) -> 
@@ -38,103 +22,61 @@ let parserTest bound parser  =
 
 let stringParser psr = parserTest 6 psr
 
-let [<Test>] ``parses all basic strings`` () =
-    longCheck <| Prop.forAll basic_string_arb (stringParser basic_string)
+let [<Property(MaxTest = 1000)>] ``parses all basic strings`` () =
+    Prop.forAll basic_string_arb (stringParser basic_string)
+
+let [<Property(MaxTest = 1000)>] ``parses all multi strings`` () =
+    Prop.forAll multi_string_arb (stringParser multi_string)
+
+let [<Property(MaxTest = 1000)>] ``parses all literal strings`` () =
+    Prop.forAll literal_string_arb (stringParser literal_string)
+
+let [<Property(MaxTest = 1000)>] ``parses all multi literal strings`` () =
+    Prop.forAll multi_lit_string_arb (stringParser multi_literal_string)
+
+let [<Property(MaxTest = 500)>] ``unified string parser reads all toml string types`` () =
+    Prop.forAll toml_string_arb (stringParser toml_string)
 
 
-let [<Test>] ``parses all multi strings`` () =
-    longCheck <| Prop.forAll multi_string_arb (stringParser multi_string)
-    
-
-let [<Test>] ``parses all literal strings`` () =
-    longCheck <| Prop.forAll literal_string_arb (stringParser literal_string)
-
-
-let [<Test>] ``parses all multi literal strings`` () =
-    longCheck <| Prop.forAll multi_lit_string_arb (stringParser multi_literal_string)
-
-
-let [<Test>] ``unified string parser reads all toml string types`` () =
-    midCheck <| Prop.forAll toml_string_arb (stringParser toml_string)
-
-
-(*|---------------------------|*)
-(*| Simple Value Parser Tests |*)
-(*|---------------------------|*)
-
+// Simple Value Parser Tests
 
 let valueParser psr = parserTest 3 psr
 
-let [<Test>] ``parses all ints`` () =
-    Check.QuickThrowOnFailure <| Prop.forAll toml_int_arb (valueParser toml_int)
+let [<Fact>] ``parses all ints`` () =
+    Prop.forAll toml_int_arb (valueParser toml_int)
+    |> Check.QuickThrowOnFailure
+
+let [<Fact>] ``parses all floats`` () =
+    Prop.forAll toml_float_arb (valueParser toml_float)
+    |> Check.QuickThrowOnFailure
+
+let [<Property>] ``parses bools`` () =
+    Prop.forAll toml_bool_arb (valueParser toml_bool )
+
+let [<Fact>] ``parses all DateTimes`` () =
+    Prop.forAll toml_datetime_arb (valueParser toml_datetime)
+    |> Check.QuickThrowOnFailure
+
+let [<Property>] ``parses all Arrays`` () =
+    Prop.forAll toml_array_arb (valueParser toml_array)
+    |> Check.QuickThrowOnFailure
 
 
-let [<Test>] ``parses all floats`` () =
-    Check.QuickThrowOnFailure <| Prop.forAll toml_float_arb (valueParser toml_float)
+// Table Parser Tests 
 
+let [<Property>] ``parses keys for table elements `` () =
+    Prop.forAll toml_key_arb (valueParser toml_key)
 
-let [<Test>] ``parses bools`` () =
-    Check.Quick <| Prop.forAll toml_bool_arb (valueParser toml_bool )
+let [<Property>] ``parses bare table keys`` () =
+    Prop.forAll toml_bareTableKey_arb (valueParser pBareKey)
 
+let [<Property>] ``parses quote table keys`` () =
+    Prop.forAll toml_quoteTableKey_arb (valueParser pQuoteKey)
 
-let [<Test>] ``parses all DateTimes`` () =
-    Check.QuickThrowOnFailure <| Prop.forAll toml_datetime_arb (valueParser toml_datetime)
+let [<Property>] ``parses toml keys`` () =
+    Prop.forAll toml_key_arb (valueParser toml_key)
 
-
-let [<Test>] ``parses all Arrays`` () =
-    Check.QuickThrowOnFailure <| Prop.forAll toml_array_arb (valueParser toml_array)
-
-
-(*|--------------------|*)
-(*| Table Parser Tests |*)
-(*|--------------------|*)
-
-
-let [<Test>] ``parses keys for table elements `` () =
-    shortCheck <| Prop.forAll toml_key_arb (valueParser toml_key)
-
-
-let [<Test>] ``parses bare table keys`` () =
-    shortCheck <| Prop.forAll toml_bareTableKey_arb (valueParser pBareKey)
-
-
-let [<Test>] ``parses quote table keys`` () =
-    shortCheck <| Prop.forAll toml_quoteTableKey_arb (valueParser pQuoteKey)
-
-
-let [<Test>] ``parses toml keys`` () =
-    shortCheck <| Prop.forAll toml_key_arb (valueParser toml_key)
-
-
-let [<Test>] ``parses toml items (key value pairs)`` () =
-    Check.QuickThrowOnFailure <| Prop.forAll toml_item_arb (valueParser toml_item)
-
-
-
-#if INTERACTIVE
-// Test Switches
-let stringTests      = false
-let simpleValueTests = false
-
-if stringTests then 
-    ``parses all basic strings`` ()
-    ``parses all multi strings`` ()
-    ``parses all literal strings`` ()
-    ``parses all multi literal strings`` () 
-    ``unified string parser reads all toml string types`` ()
-
-if simpleValueTests then
-    ``parses all ints`` ()
-    ``parses all floats`` ()
-    ``parses bools`` () 
-    ``parses all DateTimes``()
-    ``parses all Arrays`` ()
-
-
-``parses keys for table elements `` ()
-``parses bare table keys`` ()
-``parses quote table keys`` () 
-``parses toml keys`` () 
-``parses toml items (key value pairs)`` ()
-#endif
+let [<Fact>] ``parses toml items (key value pairs)`` () =
+    Prop.forAll toml_item_arb (valueParser toml_item)
+    |> Check.QuickThrowOnFailure
 
